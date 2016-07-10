6a6974c2f72de7abdd7f4920a342ca1894b6eeca
cnspraveen <cns.praveen@gmail.com>
2016-07-10 15:12:48 +0530

    Update EclipseCode_toGitHubClass.java

--- src/EclipseCode_toGitHubPackage/EclipseCode_toGitHubClass.java
+++ src/EclipseCode_toGitHubPackage/EclipseCode_toGitHubClass.java
@@ -4,7 +4,9 @@ public class EclipseCode_toGitHubClass {
 
 	public static void main(String[] args) 
 	{
-	    System.out.println("awesome");
+	    System.out.println("awesome from Eclipse");
+	    System.out.println("awesome from Git");
+	    
 	}
 
 }
