730a5aada8681336cbb98e93f7891ca97d2fba25
cnspraveen <cns.praveen@gmail.com>
2016-07-10 14:58:33 +0530

    2 programs were added in new project (EclipseCode_toGitHub)

---
+++ src/EclipseCode_toGitHubPackage/EclipseCode_toGitHubClass.java
@@ -0,0 +1,10 @@
+package EclipseCode_toGitHubPackage;
+
+public class EclipseCode_toGitHubClass {
+
+	public static void main(String[] args) 
+	{
+	    System.out.println("awesome");
+	}
+
+}
---
+++ src/EclipseCode_toGitHubPackage/EclipseCode_toGitHubClass2.java
@@ -0,0 +1,11 @@
+package EclipseCode_toGitHubPackage;
+
+public class EclipseCode_toGitHubClass2 {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("awesomw2");
+
+	}
+
+}
